cda8d97db6060be9861e3c344c43720c239db6e1
codemitrayt <codemitrayt@gmail.com>
2024-08-06 00:16:54 +0530

    add config provider for change antd primary color

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ConfigProvider } from "antd";
 
 import App from "./app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider
+    theme={{
+      token: { colorPrimary: "#09B5D3" },
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ConfigProvider>
 );
